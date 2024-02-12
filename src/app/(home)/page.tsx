60d6dbf7643d86404f139c40bc258fb410f5e428
Glaucoed <gesverzuthe@gmail.com>
2024-02-12 11:05:29 -0300

    feat: add header component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+
+import Header from "@/app/_components/header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
---
+++ src/app/_components/header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+
+export default function header() {
+  return (
+    <Card >
+      <CardContent className="p-5 flex justify-between items-center">
+        <Image src="/logo.png" alt="Logo" width={130} height={22} />
+        <Button variant="outline" size="icon" className="h-8 w-8" >
+          <MenuIcon size={18} />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
