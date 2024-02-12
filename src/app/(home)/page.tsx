456c1dfbfd9eede7e783ad33feef87c0f352e22e
Glaucoed <gesverzuthe@gmail.com>
2024-02-12 11:15:37 -0300

    feat: welcome message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,6 +5,19 @@ export default function Home() {
   return (
     <main>
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="font-bold text-xl">Olá, Glauco!</h2>
+        <p className="capitalize text-sm">
+          {
+            new Intl.DateTimeFormat("pt-BR", {
+              weekday: "long",
+              month: "long",
+              day: "numeric",    
+            }).format(new Date())
+          }
+        </p>
+      </div>
+
     </main>
   );
 }
